cd66273bbe391b6ca1745fcf28ebbc1f1df95705
MediocreDude1 <97664307+MediocreDude1@users.noreply.github.com>
2024-04-11 10:53:23 +1000

    Add files via upload

---
+++ program (2).py
@@ -0,0 +1,51 @@
+#Imports the story acts so that they can be organised.
+import random
+import time
+from ActOne import *
+from ActTwo import *
+from ActThree import *
+
+
+
+#Input for character name which will be used in rest of the story.
+player_name = input("Enter your name: ")
+
+#Begins game from ActOne
+act_one_result = ActOne(player_name)
+
+if act_one_result=='survived':
+  act_two_result = ActTwo(player_name)
+  
+  if act_two_result=='survived ActTwo':
+    act_three_result = ActThree(player_name)
+
+
+
+
+
+
+
+  
+
+
+  
+          
+          
+    
+
+
+
+  
+
+  
+  
+    
+
+
+
+    
+    
+
+    
+   
+  
